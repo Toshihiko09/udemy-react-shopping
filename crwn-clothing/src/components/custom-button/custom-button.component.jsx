2346e1444aea44f98e5aecc01e7430b6ba619bd1
Toshihiko Komuro <toshihiko.komuro.jp@gmail.com>
2020-02-11 11:11:43 +0900

    implemented firebase utils, included ability to store authenticated users into my firebase database

--- crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -6,6 +6,7 @@ import './custom-button.styles.scss';
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
   <button 
     className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`}
+    //isGoogleSignInがtrueの時、google-sign-inクラスをレンダーする
       {...otherProps}
     >
       {children}
